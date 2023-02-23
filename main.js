51659a242579431b4c14ef05fd70cf8c139efbcb
manojprabaker <manojprabaker999@gmail.com>
2023-02-23 10:20:06 +0530

    add

---
+++ main.js
@@ -0,0 +1,76 @@
+document.write("hai");
+
+let a=1;
+console.log(a);
+
+var A=2;
+console.log(A);
+
+const s="MJ2";
+console.log(s);
+
+let b=4;
+let c=b+a;
+console.log(c);
+
+let bn=5562362n;
+let bn1=3543645n;
+console.log(bn+bn1);
+
+
+ var areurobot=false;
+console.log(typeof areurobot);
+
+console.log(typeof b);
+console.log(typeof a);
+console.log(typeof s);
+
+let user;
+console.log(user);
+
+let user2=null;
+console.log(typeof user2);
+
+// confirm(); 
+
+//alert("hai");
+
+var p=1; 
+var q=2;
+
+var r=p;
+var p=q;
+var q=r;
+console.log("p is " + p);
+console.log("q is " + q);
+
+
+var a1=10, b1=560n;
+console.log(typeof b1);
+
+let str=254734747457565679;
+console.log(typeof str);
+
+
+var ab=2;
+var ac=4;
+console.log(typeof ac);
+console.log(ab+ac);
+
+  
+ 
+  var ab2="manoj";
+  console.log(typeof ab2);
+
+
+var a2b=45;
+var a2b=46;
+console.log(a2b);
+
+
+console.log(abc);
+let abc=44;
+
+
+
+
